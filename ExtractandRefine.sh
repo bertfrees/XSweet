043409603d61cc69322d11881a94a2f4dee7faa9
Wendell Piez <wapiez@wendellpiez.com>
2016-09-21 16:35:47 -0400

    Touchups and comments

--- ExtractandRefine.sh
+++ ExtractandRefine.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 # For producing HTML5 outputs via XSweet XSLT from sources extracted from .docx (Office Open XML)
 
+# $DOCNAME is any short identifier
+DOCNAME="DEMOTEXT"                                          
+
 # $DOCXdocumentXML is the 'word/document.xml' file extracted (unzipped) from a .docx file
 # (Also, its neighbor files from the .docx package should be available.)
 DOCXdocumentXML="Demo_text_docx/word/document.xml"
 
-# $FILE is a short identifier
-FILE="DEMOTEXT"                                          
+# Bind $DOCXdocumentXML and $DOCNAME via $1 and $2 and try
+# CL > ./ExtractandRefine.sh yourdocumentname path/to/your/document.xml
+# (Which would make it possible to call this script from another one
+# and even loop over file sets.)
 
+# Note Saxon is included with this distribution, qv for license.
 saxonHE="java -jar lib/SaxonHE9-7-0-8J/saxon9he.jar"  # SaxonHE (XSLT 2.0 processor)
-EXTRACT="docx-html-extract.xsl"        # "Extraction" stylesheet
-REFINE1="handle-notes.xsl"             # "Refinement" stylesheets
+EXTRACT="docx-html-extract.xsl"                       # "Extraction" stylesheet
+REFINE1="handle-notes.xsl"                            # "Refinement" stylesheets
 REFINE2="scrub.xsl"
 REFINE3="join-elements.xsl"
 
-# Intermediate and final outputs (serializations) are all left on the file system
-$saxonHE -xsl:$EXTRACT -s:$DOCXdocumentXML     -o:$FILE-EXTRACTED.html
-echo Made $FILE-EXTRACTED.html
-$saxonHE -xsl:$REFINE1 -s:$FILE-EXTRACTED.html -o:$FILE-REFINED_1.html
-echo Made $FILE-REFINED_1.html
-$saxonHE -xsl:$REFINE2 -s:$FILE-REFINED_1.html -o:$FILE-REFINED_2.html
-echo Made $FILE-REFINED_2.html
-$saxonHE -xsl:$REFINE3 -s:$FILE-REFINED_2.html -o:$FILE-REFINED_3.html
-echo Made $FILE-REFINED_3.html
+# Intermediate and final outputs (serializations) are all left on the file system.
+
+$saxonHE -xsl:$EXTRACT -s:$DOCXdocumentXML        -o:$DOCNAME-EXTRACTED.html
+echo Made $DOCNAME-EXTRACTED.html
+
+$saxonHE -xsl:$REFINE1 -s:$DOCNAME-EXTRACTED.html -o:$DOCNAME-REFINED_1.html
+echo Made $DOCNAME-REFINED_1.html
+
+$saxonHE -xsl:$REFINE2 -s:$DOCNAME-REFINED_1.html -o:$DOCNAME-REFINED_2.html
+echo Made $DOCNAME-REFINED_2.html
+
+$saxonHE -xsl:$REFINE3 -s:$DOCNAME-REFINED_2.html -o:$DOCNAME-REFINED_3.html
+echo Made $DOCNAME-REFINED_3.html
 
